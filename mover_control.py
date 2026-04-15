ecbfca9781926b72dcf0ceaf35ff955e91d186b1
naomipark3 <naomi_park@alumni.brown.edu>
2026-04-15 09:45:44 -0700

    ready for demo

--- mover_control.py
+++ mover_control.py
@@ -429,6 +429,7 @@ if __name__ == "__main__":
         local_ip="172.24.68.147",
     )
     system.connect()
+    #system.initialize()
 
     try:
         positions = system.get_all_mover_positions()
@@ -436,7 +437,7 @@ if __name__ == "__main__":
             print(f"Mover {m} at ({px:.1f}, {py:.1f})")
 
         #Example: move mover 1 to a target, routing around mover 2 if needed
-        system.smart_move_to(2, 180.0, 400, velocity=30)
+        system.smart_move_to(2, 180.0, 350.0, velocity=10)
 
     finally:
         system.disconnect()
